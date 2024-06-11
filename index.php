ce442d6b540bdcb2e8c9c90967e07d8394a5034b
Christian <joaochristian03@gmail.com>
2024-06-11 20:43:26 -0300

    Adicao css e php

--- index.php
+++ index.php
@@ -15,7 +15,37 @@
 <body>
     <div class="quadrado">
         <h1>IDENTIFICADOR DE NÚMEROS PRIMOS</h1>
+        <form method="get" action="index.php">
+            <label for="">Digite o Número para saber se ele é Primo ou não:</label><br>
+            <input type="number" placeholder="Ex: 2" class="campo" name="primo" min="1" required><br>
+            <a href="index.php">
+                <input type="submit" value="Verificar" class="verificar">
+            </a>
     </div>
+
+    <?php
+    $primo = $_GET["primo"];
+
+    function ehPrimo($num)
+    {
+        if ($num <= 1) {
+            return false;
+        }
+        for ($i = 2; $i <= sqrt($num); $i++) {
+            if ($num % $i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    if (ehPrimo($primo)) {
+        echo " <div class='Resultado'>O número $primo é Primo!</div>";
+    } else {
+        echo " <div class='Resultado'>O número $primo NÃO é Primo!</div>";
+    }
+
+    ?>
 </body>
 
 </html>
